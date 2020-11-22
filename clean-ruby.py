f0d479ea7eae3f223db8e00a048c6653beb4e975
Daniel M German <dmg@turingmachine.org>
2020-11-21 21:23:58 -0800

    first release

---
+++ clean-ruby.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+import sys
+import os.path
+
+from bs4 import BeautifulSoup
+
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Usage %s <filename>\n"%(sys.argv[0]))
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+if not os.path.isfile(filename):
+    sys.stderr.write("File [%s] did not exist\n"%(filename))
+    sys.stderr.write("Usage %s <filename>\n"%(sys.argv[0]))
+    sys.exit(1)
+
+html = open(filename).read()
+soup = BeautifulSoup(html,'html.parser')
+
+
+# replace with `soup.findAll` if you are using BeautifulSoup3
+for rt in soup.find_all("rt"): 
+    rt.decompose()
+for ruby in soup.find_all("ruby"): 
+    ruby.unwrap()
+
+print (soup)
+
